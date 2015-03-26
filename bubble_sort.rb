2d5427067f64e00d38fe0d6a61c810d7a96423a0
Antonio Augusto <antonioabritto@gmail.com>
2015-03-26 10:21:53 -0300

    refactor #bubble_sort to use the block version

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,9 @@
 class BubbleSort
   
   def bubble_sort ary
-    raise ArgumentError unless ary.is_a? Array
-    swap = true
-    while swap
-      swap = false
-      (ary.length-1).times do |index|
-        if ary[index] > ary[index+1]
-          ary[index], ary[index+1] = ary[index+1], ary[index]
-          swap = true
-        end
-      end
+    bubble_sort_by ary do |left, right|
+      right - left
     end
-    ary
   end
 
   def bubble_sort_by ary
